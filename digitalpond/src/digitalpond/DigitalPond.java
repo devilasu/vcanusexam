c87cb1413dfe564de72f2d397e52885304ec69c3
KYJ <devilasu@naver.com>
2021-09-11 16:26:02 +0900

    Fix: 수정: 가변성 추가

--- digitalpond/src/digitalpond/DigitalPond.java
+++ digitalpond/src/digitalpond/DigitalPond.java
@@ -4,7 +4,16 @@ public class DigitalPond {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Pond pond = new Pond();	//지도 데이터가 있는 클래스 객체 생성
+		Pond pond = new Pond( "0 0 0 0 0 0 0 0 0 0 0\n"
+				+ "0 0 0 0 1 0 0 0 0 0 0\n"
+				+ "0 0 0 1 1 1 0 0 0 0 0\n"
+				+ "0 1 1 1 1 1 1 0 0 0 0\n"
+				+ "0 1 1 1 1 1 1 1 1 0 0\n"
+				+ "0 1 1 1 1 1 1 1 1 0 0\n"
+				+ "0 0 1 1 1 1 1 1 0 0 0\n"
+				+ "0 0 0 1 1 1 1 0 0 0 0\n"
+				+ "0 0 0 0 1 0 0 0 0 0 0\n"
+				+ "0 0 0 0 0 0 0 0 0 0 0");	//지도 데이터가 있는 클래스 객체 생성
 		pond.calcMap();	//초기 지도를 수정하는 메소드
 		pond.printMap();	//지도의 결과를 출력하는 메소드
 	}
--- digitalpond/src/digitalpond/Pond.java
+++ digitalpond/src/digitalpond/Pond.java
@@ -3,22 +3,15 @@ package digitalpond;
 import java.util.Arrays;
 
 public class Pond {//맵 데이터를 제어하기 위한 클래스
-	int[][] map = new int[10][10];
-	public Pond() {
-		//제공한 맵 데이터를 카피했다.
-		String tmp = "0 0 0 0 0 0 0 0 0 0\n"
-				+ "0 0 0 0 1 0 0 0 0 0\n"
-				+ "0 0 0 1 1 1 0 0 0 0\n"
-				+ "0 1 1 1 1 1 1 0 0 0\n"
-				+ "0 1 1 1 1 1 1 1 1 0\n"
-				+ "0 1 1 1 1 1 1 1 1 0\n"
-				+ "0 0 1 1 1 1 1 1 0 0\n"
-				+ "0 0 0 1 1 1 1 0 0 0\n"
-				+ "0 0 0 0 1 0 0 0 0 0\n"
-				+ "0 0 0 0 0 0 0 0 0 0";
+	int width;
+	int height;
+	int[][] map;
+	public Pond(String tmp) {
 		
 		String[] tmpArray = tmp.split("\n");
-		
+		height = tmpArray.length;				//맵 데이터로부터 width값 할당.
+		width = tmpArray[0].split(" ").length;	//맵 데이터로부터 height값 할당.
+		map = new int[width][height];			//맵 데이터로부터 얻어온 값으로 heap에 배열 생성.
 		for(int i = 0; i < tmpArray.length;i++) {
 			//String 지도 정보를 int로 파싱.
 			map[i]= Arrays.stream(tmpArray[i].split(" ")).mapToInt(Integer::parseInt).toArray();
@@ -31,8 +24,8 @@ public class Pond {//맵 데이터를 제어하기 위한 클래스
 		boolean changed = true;
 		while(changed) {
 			changed=false;
-			for(int i = 0 ; i<map.length;i++) {
-				for(int j = 0 ; j<map.length;j++) {
+			for(int i = 0 ; i<height;i++) {
+				for(int j = 0 ; j<width;j++) {
 					if(isDepth(i,j)) {//좌,우,상,하 체크
 						changed = true;
 						map[i][j] = map[i][j]+ 1;//깊이 +1
@@ -43,8 +36,8 @@ public class Pond {//맵 데이터를 제어하기 위한 클래스
 	}
 	private boolean isDepth(int i, int j) {
 		int depth = map[i][j];
-		if(i>0 && i<map.length-1) {	//상,하 체크를 위한 범위 설정
-			if(j>0 && j<map[i].length-1) {//좌,우 체크를 위한 범위 설정
+		if(i>0 && i<height-1) {	//상,하 체크를 위한 범위 설정
+			if(j>0 && j<width-1) {//좌,우 체크를 위한 범위 설정
 				if(depth!=0) {//땅이 아닐 경우
 					if(map[i-1][j]>=depth) {//상 체크
 						if(map[i+1][j]>=depth) {//하 체크
@@ -64,16 +57,16 @@ public class Pond {//맵 데이터를 제어하기 위한 클래스
 	public void printMap() {//맵을 출력하는 기능
 		String txt = "";
 		int sum = 0;
-		for(int i = 0; i<map.length;i++) {
+		for(int i = 0; i<height;i++) {
 			txt = "";
-			for(int j = 0 ; j<map[i].length;j++) {
+			for(int j = 0 ; j<width;j++) {
 				txt += map[i][j]+" ";
 				if(map[i][j]!=0)
 					sum += map[i][j];
 			}
 			System.out.println(txt);
 		}
-		System.out.println(sum);
+		System.out.println("width: "+width+", height: "+height+", sum: "+sum);
 	}
 	
 	
